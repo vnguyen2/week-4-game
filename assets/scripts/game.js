432b839661f30d882691b2eb8b8ddccccf7ff801
Vincent Nguyen <vnguyen2@gmail.com>
2016-08-24 18:48:08 -0500

    character selection working

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,45 +1,53 @@
 
 //defining characters of game
 var	pikachu =  {
-		name: "Pikachu",
-		hitpoints: 120,
-		attackPower: 8,
-};
-var	pikachuStats = $(".pikachu");
-	pikachuStats.attr({name: pikachu.name}, {hitpoints: pikachu.hitpoints});
-	// pikachuStats.attr('hitpoints', pikachu.hitpoints);
-	pikachuStats.attr('attackPower', pikachu.attackPower);	
-	
-	
-var	character2 = {
-		name: "Squirtle",
-		hitpoints: 100,
-		attackPower: 5,
+	name: "Pikachu",
+	hitpoints: 120,
+	attackPower: 8,
+},
+	pikachuStats = $(".pikachu");
+	pikachuStats.attr({name: pikachu.name}, {hitpoints: pikachu.hitpoints}, {attackPower: pikachu.attackPower});
+
+var	squirtle = {
+	name: "Squirtle",
+	hitpoints: 100,
+	attackPower: 5,
+},
+	squirtleStats = $(".squirtle");
+	squirtleStats.attr({name: squirtle.name}, {hitpoints: squirtle.hitpoints}, {attackPower: squirtle.attackPower});
+
+var	charmander = {
+	name: "Charmander",
+	hitpoints: 150,
+	attackPower: 20,
+},
+	charmanderStats = $(".charmander");
+	charmanderStats.attr({name: charmander.name}, {hitpoints: charmander.hitpoints}, {attackPower: charmander.attackPower});
+
+
+var	bulbasaur = {
+	name: "Bulbasaur",
+	hitpoints: 180,
+	attackPower: 25,
 	},
-	character3 = {
-		name: "Charmander",
-		hitpoints: 150,
-		attackPower: 20,
-	},
-	character4 = {
-		name: "Bulbasaur",
-		hitpoints: 180,
-		attackPower: 25,
-	};
+	bulbasaurStats = $(".bulbasaur");
+	bulbasaurStats.attr({name: bulbasaur.name}, {hitpoints: bulbasaur.hitpoints}, {attackPower: bulbasaur.attackPower});
+
 
 		
-
 
 $(function() {
 	//pick Character to play and move to userChar div
-	$(".charChoice").on('click', function() {
+	$('.charChoice').on('click', function() {
 		//this allows only selected char to have class removed
-		$(this).removeClass("nonCharChoice")
-		$(".nonCharChoice").removeClass("charChoice")
-			.appendTo(".enemies")
-		$(".userPick").html("You chose " + $(".charChoice").attr('name') + " as your Pokemon.");
+		$(this).removeClass('nonCharChoice')
+		$('.nonCharChoice').removeClass('charChoice')
+			.appendTo('.enemies')
+		$('.userPick').html("You chose " + $('.charChoice').attr('name') + " as your Pokemon.");
 		
 	});
+
+	$(".nonCharChoice").on('click')
 });
 
 	
